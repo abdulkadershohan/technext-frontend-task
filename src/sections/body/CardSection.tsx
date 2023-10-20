d1bfa3cf9fadb832fe04d23fbc370d8626d8fbed
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-10-20 21:43:30 +0600

    pagination done

--- src/sections/body/CardSection.tsx
+++ src/sections/body/CardSection.tsx
@@ -35,6 +35,95 @@ export default function CardSection() {
                     ))
                 }
             </div>
+
+            <nav
+                className="flex items-center justify-center mt-8"
+            >
+                <ul className="flex items-center -space-x-px h-10 text-base">
+                    <li>
+                        <p
+                            className="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg  hover:text-[#fff] bg-white   cursor-pointer "
+                            onClick={() => {
+                                if (offset > 1) {
+                                    setOffset(offset - 1)
+                                }
+
+                            }}
+                        >
+                            <span className="sr-only">Previous</span>
+                            <svg className="w-3 h-3  text-primary " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                            </svg>
+                        </p>
+                    </li>
+                    <li>
+                        <p
+
+                            className={styleActive}
+                        //  onClick={() => setOffset(1)}
+
+                        >{offset}</p>
+                    </li>
+                    <li>
+                        <p
+                            className={style}
+                            onClick={() => setOffset((prev) => prev + 1)}
+
+                        >{offset + 1}</p>
+                    </li>
+
+                    <li>
+                        <p
+                            className={style}
+                            onClick={() => setOffset((prev) => prev + 2)}
+
+                        >{offset + 2}</p>
+                    </li>
+                    <li>
+                        <p
+
+                            className={style}
+                            onClick={() => setOffset((prev) => prev + 3)}
+                        >{offset + 3}</p>
+                    </li>
+                    <li>
+                        <p
+
+                            className={style}
+                        >...</p>
+                    </li>
+                    <li>
+                        <p
+                            className={style}
+                            onClick={() => setOffset((prev) => prev + 10)}
+                        >{offset + 10}</p>
+                    </li>
+                    <li>
+                        <p
+                            className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg    text-[#fff] cursor-pointer"
+                            onClick={() => setOffset(offset + 1)}
+                        >
+                            <span className="sr-only">Next</span>
+                            <svg className="w-3 h-3 text-primary " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                            </svg>
+                        </p>
+                    </li>
+                </ul>
+            </nav>
         </section>
     )
 }
+
+const style = `flex items-center justify-center px-4 h-10 leading-tight 
+text-primary bg-white border
+ border-gray-300
+  hover:text-[#fff]
+   hover:bg-primary cursor-pointer
+   text-[14px] font-normal`
+const styleActive = `flex items-center justify-center px-4 h-10 leading-tight 
+text-primary bg-white border
+ border-gray-300
+  text-[#fff]
+   bg-primary
+   text-[14px] font-normal`
