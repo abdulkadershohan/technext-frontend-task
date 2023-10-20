8c7e8615f98614565051f8fba55fcec78ed66eeb
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-10-20 23:28:11 +0600

    FilterSearchContextType added

--- src/context/filterSearchContext.ts
+++ src/context/filterSearchContext.ts
@@ -1,15 +1,4 @@
 import { createContext } from "react";
-
-export interface FilterSearchContextType {
-    search?: string;
-    setSearch?: (search: string) => void;
-    isUpcoming?: boolean;
-    setIsUpcoming?: (isUpcoming: boolean) => void;
-    filterByStatus?: string;
-    setFilterByStatus?: (filterByStatus: string) => void;
-    filterByDate?: string;
-    setFilterByDate?: (filterByDate: string) => void;
-
-}
+import { FilterSearchContextType } from "src/interfaces/filter";
 
 export const filterSearchContext = createContext<FilterSearchContextType>({});
--- src/interfaces/filter.ts
+++ src/interfaces/filter.ts
@@ -9,3 +9,14 @@ export interface ISelectDate {
     name: string;
     value: string;
 }
+export interface FilterSearchContextType {
+    search?: string;
+    setSearch?: (search: string) => void;
+    isUpcoming?: boolean;
+    setIsUpcoming?: (isUpcoming: boolean) => void;
+    filterByStatus?: string;
+    setFilterByStatus?: (filterByStatus: string) => void;
+    filterByDate?: string;
+    setFilterByDate?: (filterByDate: string) => void;
+
+}
