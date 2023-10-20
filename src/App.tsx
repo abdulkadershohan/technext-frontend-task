46d72bc56b0c797dce4ee710594ef14326603d84
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-10-20 23:33:20 +0600

    useValue hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
 import { filterSearchContext } from "./context/filterSearchContext";
+import useValue from "./hooks/useValue";
 import HomePage from "./page/home/HomePage";
 
 function App() {
-  const [check, setCheck] = useState<boolean>(false)
-  const [filterByDate, setFilterByDate] = useState('')
-  const [filterByStatus, setFilterByStatus] = useState('')
-  const [search, setSearch] = useState('')
-  const [isUpcoming, setIsUpcoming] = useState<boolean>(false)
-  const value = {
-    check,
-    setCheck,
-    filterByDate,
-    setFilterByDate,
-    filterByStatus,
-    setFilterByStatus,
-    search,
-    setSearch,
-    isUpcoming,
-    setIsUpcoming
-  }
+  const value = useValue()
   return (
     <filterSearchContext.Provider value={value}>
       <main className="bg-main">
---
+++ src/hooks/useValue.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function useValue() {
+    const [check, setCheck] = useState<boolean>(false)
+    const [filterByDate, setFilterByDate] = useState('')
+    const [filterByStatus, setFilterByStatus] = useState('')
+    const [search, setSearch] = useState('')
+    const [isUpcoming, setIsUpcoming] = useState<boolean>(false)
+    const value = {
+        check,
+        setCheck,
+        filterByDate,
+        setFilterByDate,
+        filterByStatus,
+        setFilterByStatus,
+        search,
+        setSearch,
+        isUpcoming,
+        setIsUpcoming
+    }
+    return value
+
+}
