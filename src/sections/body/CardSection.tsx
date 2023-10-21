9b449104acb4203e9793e7e16547eba55a9a509e
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-10-21 20:13:02 +0600

    completed

--- src/sections/body/CardSection.tsx
+++ src/sections/body/CardSection.tsx
@@ -35,6 +35,7 @@ export default function CardSection() {
         catch (error) {
             console.log(error)
             setData([])
+            setLoading(false)
         }
     }, [offset, limit, search, check, filterByStatus])
 
